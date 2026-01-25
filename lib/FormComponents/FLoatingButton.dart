81094ecb2551c3fd78d14dc18f22856a36536f71
Kuldeep Kishan Kolage <kolagekuldeep09@gmail.com>
2026-01-25 12:08:13 +0530

    Bottom Nav

--- lib/FormComponents/FLoatingButton.dart
+++ lib/FormComponents/FLoatingButton.dart
@@ -3,12 +3,22 @@ import 'package:flutter/material.dart';
 class FloatingButton extends StatelessWidget {
   final VoidCallback onPressed;
 
-  const FloatingButton({super.key, required this.onPressed});
+  const FloatingButton({
+    super.key,
+    required this.onPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: onPressed,
+
+      // ✅ REMOVE ALL SHADOW / ELEVATION
+      elevation: 0,
+      highlightElevation: 0,
+      focusElevation: 0,
+      hoverElevation: 0,
+
       backgroundColor: Colors.yellow,
       shape: const CircleBorder(),
       child: const Icon(
